f46836aedb847f6b32dfb5b0fe378b9bcbb33735
sahil-SS <125299733+sahil-SS@users.noreply.github.com>
2023-09-15 12:33:30 +0530

    [sahil] form class package

---
+++ src/Class/xyz.java
@@ -0,0 +1,14 @@
+package Class;
+
+public class xyz {
+	
+	public static void m1() {
+	System.out.println("m1 is static method");	
+		
+	}
+	
+	public static void main(String[] args) {
+		m1();
+	}
+	}
+
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module company {
+	exports selenium;
+}
